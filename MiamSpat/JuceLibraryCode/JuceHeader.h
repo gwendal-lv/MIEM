0426305aefa47508e046090aed58cdee7ce87b7e
Gwendal Le Vaillant <gwendal.levaillant@gmail.com>
2017-10-09 22:12:04 +0200

    Début de remise en fonction du MiamSpat

--- MiamSpat/JuceLibraryCode/JuceHeader.h
+++ MiamSpat/JuceLibraryCode/JuceHeader.h
@@ -10,8 +10,7 @@
 
 */
 
-#ifndef __APPHEADERFILE_XCUIGI__
-#define __APPHEADERFILE_XCUIGI__
+#pragma once
 
 #include "AppConfig.h"
 
@@ -44,5 +43,3 @@ namespace ProjectInfo
     const int          versionNumber  = 0x10000;
 }
 #endif
-
-#endif   // __APPHEADERFILE_XCUIGI__
