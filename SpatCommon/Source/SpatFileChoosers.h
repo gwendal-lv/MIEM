1be2d80d7a1290e3c2a04666919fecd329393b99
Gwendal Le Vaillant <gwendal.levaillant@gmail.com>
2017-11-20 15:06:50 +0100

    Ajout des sessions dans le repo + classes pour accéder direct au dossier du repo (pour compilation en debug)

--- SpatCommon/Source/SpatFileChoosers.h
+++ SpatCommon/Source/SpatFileChoosers.h
@@ -10,21 +10,62 @@
 
 #pragma once
 
+#include <boost/predef.h> // OS-specific defines
+
 #include "JuceHeader.h"
 
+#include <string>
 
 #define Miam_SessionFileExtension       "mspat"
 
-
 namespace Miam
 {
+    
+    class PathUtils
+    {
+        public :
+        
+        static std::string GetAppToRepositoryRootPath()
+        {
+            // Chemin par rapport aux exécutables : dépend de l'OS
+#if BOOST_OS_WINDOWS // Car chemin de la forme : MiamXyz/Builds/VisualStudio2015/x64/Debug/App
+            return "../../../../../../";
+#elif BOOST_OS_MACOS // Car chemin de la forme : MiamXyz/Builds/MacOSX/build/Debug/
+            return "../../../../../";
+#else
+            return "";
+#endif
+        }
+        
+        static std::string GetAppToRepositorySessionsPath()
+        {
+            if (GetAppToRepositoryRootPath() == "")
+                throw std::logic_error("Path to repo root is not defined. Cannot continue");
+            else
+                return GetAppToRepositoryRootPath() + "SpatCommon/Sessions/";
+        }
+        
+        static File GetSessionsFolderDefaultPath()
+        {
+#ifdef __MIAM_DEBUG // Debug : sessions
+#if defined(BOOST_OS_WINDOWS) || defined(BOOST_OS_MACOS) // Debug DESKTOP : on cherche les sessions dans le repo
+            return File(GetAppToRepositorySessionsPath());
+#else // Debug MOBILE : on balance le dossier musique par défaut
+            return File::getSpecialLocation(File::SpecialLocationType::userMusicDirectory);
+#endif
+#else // Release : dossier musique, par défaut
+            return File::getSpecialLocation(File::SpecialLocationType::userMusicDirectory);
+#endif
+        }
+    };
+    
     
     class LoadFileChooser : public FileChooser
     {
         public :
         LoadFileChooser() :
         FileChooser("Please select a MIAM session to load: ",
-                    File::getSpecialLocation(File::SpecialLocationType::userMusicDirectory),
+                    PathUtils::GetSessionsFolderDefaultPath(),
                     std::string("*.") + Miam_SessionFileExtension,
                     true)
         {}
@@ -36,10 +77,11 @@ namespace Miam
         public :
         SaveFileChooser() :
         FileChooser("Save MIAM session as...",
-                    File::getSpecialLocation(File::SpecialLocationType::userMusicDirectory),
+                    PathUtils::GetSessionsFolderDefaultPath(),
                     std::string("*.") + Miam_SessionFileExtension,
                     true)
         {}
     };
+    
     
 }
