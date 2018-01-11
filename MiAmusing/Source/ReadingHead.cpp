ff6ccf1a9a463b56830170072e0aa5dc6b25f3fb
gvillee <guillaume.villee@irisib.be>
2018-01-11 17:16:52 +0100

    juste un peu de documentation pour pas se perdre...

--- MiAmusing/Source/ReadingHead.cpp
+++ MiAmusing/Source/ReadingHead.cpp
@@ -90,17 +90,18 @@ void PlayHead::setSpeed(double m_speed)
 				{
 					if (m_speed < 1)
 					{
-						//m_speed = 0.5;
+						
 						speedToReach = m_speed;
 						double currentPosition = position;//speed * (((double)metronome->getCurrentBeat() + 1.0) * (double)periodInSamples - metronome->getNumSamplesToNextBeat());
 														  // retenir le nombre de tour 
 						rest = speedToReach - floor(speedToReach);
-						//currentT = 0;
+						
+						// addT = décalage à ajouter à la position car la vitesse n'est pas un entier
 						int addT;
 						if (rest != 0)
 						{
 							//numT = (rest == 0) ? 1 : ceil(1.0 / rest);
-							double delta = 0.001;
+							double delta = 0.001; // on pourrait faire numT = ceil(1.0 / rest), mais avec les arrondi on peut arriver à un mauvais résultat (1/3 = 0.33333000 -> numT = 4 !!! donc faux !)
 							for (int i = 2; i < 5; ++i)
 								if (speedToReach >= 1.0 / (double)i - delta / 2.0 && speedToReach <= 1.0 / (double)i + delta / 2.0)
 									numT = i;
@@ -110,9 +111,14 @@ void PlayHead::setSpeed(double m_speed)
 						}
 						else
 							addT = 0;
+
+						// la position à atteindre choisie, est la position qu'atteindrait une tête de lecture à v = speedToReach lorsque qu'une tête de lecture à vitesse 1 atteindra la fin de sa timeLine
 						double positionToReach = numOfBeats * periodInSamples + addT * (1.0/(double)numT) * numOfBeats * periodInSamples;
-						double currentPositionAtSpeedToReach = speedToReach * (((double)metronome->getCurrentBeat() + 1.0) * (double)periodInSamples - metronome->getNumSamplesToNextBeat()) + addT * rest * numOfBeats * periodInSamples;;
+						// la position actuelle d'une tête de lecture à v = speedToReach
+						double currentPositionAtSpeedToReach = speedToReach * (((double)metronome->getCurrentBeat() + 1.0) * (double)periodInSamples - metronome->getNumSamplesToNextBeat()) + addT * rest * numOfBeats * periodInSamples;
+						// temps que mettra une tête de lecture à v = speedToReach pour atteindre la position ciblée
 						transitionTime = (positionToReach - currentPositionAtSpeedToReach) / speedToReach;
+						
 						transitionSpeed = (positionToReach - currentPosition) / transitionTime;
 					}
 					else
@@ -145,8 +151,10 @@ void PlayHead::setSpeed(double m_speed)
 				int tmpT = metronome->getCurrentT();
 				while (tmpT > numT)
 					tmpT -= numT;
+
+				// plus tient compte du décalage qu'il y aura par rapport à v = 1
 				plus = tmpT * (1.0/(double)numT) * numOfBeats * periodInSamples; //position + 1.0;
-				//numT = (rest == 0) ? 1 : ceil(1.0 / rest);
+				
 				break;
 		}
 	}
