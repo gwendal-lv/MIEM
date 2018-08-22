5793ef65fa443c902779d47a70b6ccde5d83142a
gvillee <guillaume.villee@irisib.be>
2018-08-22 13:50:26 +0200

    changement nom de l'application MiAmusing -> Amusing

--- MiAmusing/JuceLibraryCode/JuceHeader.h
+++ MiAmusing/JuceLibraryCode/JuceHeader.h
@@ -41,7 +41,7 @@
 #if ! JUCE_DONT_DECLARE_PROJECTINFO
 namespace ProjectInfo
 {
-    const char* const  projectName    = "MiAmusing";
+    const char* const  projectName    = "Amusing";
     const char* const  versionString  = "1.0.0";
     const int          versionNumber  = 0x10000;
 }
