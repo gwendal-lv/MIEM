c35e35851d703da593235502dd585e35998bb0ec
Gwendal Le Vaillant <gwendal.levaillant@gmail.com>
2019-06-16 21:08:41 +0200

    calcul de score : Plot 1 seule surface

--- Experiments/ExperimentsDataProcessing/MatlabLib/PlotPerformanceSurfaces.m
+++ Experiments/ExperimentsDataProcessing/MatlabLib/PlotPerformanceSurfaces.m
@@ -1,9 +1,15 @@
-function [] = PlotPerformanceSurfaces()
+function [] = PlotPerformanceSurfaces(surfaceName)
 %PLOTPERFORMANCESURFACES Plots the surfaces corresponding to the
 %computation methods for estimating the performance of a subject, depending
 %on the error and 
 %   Detailed explanation goes here
 
+displayAllSurfaces = false;
+if (nargin==1)
+    if strcmp(surfaceName, 'all')
+        displayAllSurfaces = true;
+    end
+end
 
 % sizes of the numeric mesh
 N = 41;
@@ -40,10 +46,13 @@ for i=1:M % searchTime
 end
 
 
-% plotS
+% plot(S)
 figure();
 
-subplot(1,2,1);
+if (displayAllSurfaces)
+    subplot(1,2,1);
+end
+
 surf(parametricError, searchTime, basicPerf);
 title('In-Game performance function');
 xlabel('Error E');
@@ -51,19 +60,23 @@ ylabel('Search duration T [s]');
 ylim( [0 maxDisplayTime] );
 zlabel('Performance P');
 %view(0,90); % top view
-view(135,45);
+view(140, 20);
 pbaspect([1 1 1]);
 
-subplot(1,2,2);
-surf(parametricError, searchTime, altPerf);
-title('Basic performance function');
-xlabel('Error E');
-ylabel('Search duration T [s]');
-ylim( [0 maxDisplayTime] );
-zlabel('Performance P');
-%view(0,90); % top view
-view(135,45);
-pbaspect([1 1 1]);
+if (displayAllSurfaces)
+    subplot(1,2,2);
+    surf(parametricError, searchTime, altPerf);
+    title('Basic performance function');
+    xlabel('Error E');
+    ylabel('Search duration T [s]');
+    ylim( [0 maxDisplayTime] );
+    zlabel('Performance P');
+    %view(0,90); % top view
+    view(140, 20);
+    pbaspect([1 1 1]);
+end
+
+colorbar
 
 
 end
